8a4c0667c4c7f663758cbac84b4ece018783f836
Ali Hadi Mazeh <alihadimazeh@cmail.carleton.ca>
2026-09-05 13:17:19 -0400

    restrict deletion of jobs and orders for a customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ApplicationRecord
   has_many :leads,  dependent: :destroy
   has_many :quotes, dependent: :destroy
-  has_many :jobs
+  has_many :jobs,   dependent: :restrict_with_error
+  has_many :orders, dependent: :restrict_with_error
 
   def full_name
     "#{first_name} #{last_name}"
