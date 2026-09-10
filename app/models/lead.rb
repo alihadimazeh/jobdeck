1fe8b5f1760a4d59c651643ad4c578b4b4c19b3f
Ali Hadi Mazeh <alihadimazeh@cmail.carleton.ca>
2026-09-09 22:55:04 -0400

    Add suffix: true to Lead.source enum

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,7 +1,7 @@
 class Lead < ApplicationRecord
   enum :status, { new: 0, contacted: 1, quoted: 2, converted: 3, lost: 4 }, suffix: true
   enum :job_type, { tile: 0, flooring: 1, materials: 2, kitchen: 3, mixed: 4 }, suffix: true
-  enum :source, { walk_in: 0, phone: 1, referral: 2, website: 3, other: 4 }
+  enum :source, { walk_in: 0, phone: 1, referral: 2, website: 3, other: 4 }, suffix: true
 
   validates :title, presence: true
 
