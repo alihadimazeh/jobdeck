008bc7dbf3a2de76ad0d5f0eeb216892511aee1c
Ali Hadi Mazeh <alihadimazeh@cmail.carleton.ca>
2026-09-10 12:42:05 -0400

    Reorder association for leads & quotes on customer
    - reordered associations to allow for `restrict_wih_error` guard to run
      before any deletes happen on lead. this allows us to avoid deletion if
    a customer has any orders or jobs

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,8 @@
 class Customer < ApplicationRecord
-  has_many :leads,  dependent: :destroy
-  has_many :quotes, dependent: :destroy
   has_many :jobs,   dependent: :restrict_with_error
   has_many :orders, dependent: :restrict_with_error
+  has_many :leads,  dependent: :destroy
+  has_many :quotes, dependent: :destroy
 
   def full_name
     "#{first_name} #{last_name}"
